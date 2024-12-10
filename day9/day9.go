e8691899afc27cbcf5e3c5ee5fa4a17e8a1db30f
Johanna Pearce <jo@nrwl.io>
2024-12-10 10:58:09 +0000

    2024-12-09, day 9, part 2 updated to use a linked list

--- day9/day9.go
+++ day9/day9.go
@@ -68,83 +68,148 @@ func calculateCheckSum(line []string) int {
 }
 
 type File struct {
-	id     int
-	length int
+	id       int
+	length   int
+	previous *File
+	next     *File
 }
 
-func expandFileSystemToFiles(line []string) []File {
-	expanded := []File{}
-	for i := range line {
+func expandFileSystemToFiles(line []string) *File {
+	var current *File = nil
+	var previous *File = nil
+	for i := 0; i < len(line); i++ {
 		// even numbers are file blocks
 		// odd numbers are free space
 		length := utils.ToInt(line[i])
 		if i%2 == 0 {
-			expanded = append(expanded, File{(i + 1) / 2, length})
+			current = &File{(i + 1) / 2, length, previous, nil}
 		} else {
-			expanded = append(expanded, File{-1, length})
+			current = &File{-1, length, previous, nil}
 		}
+		if previous != nil {
+			previous.next = current
+		}
+		previous = current
 	}
-	return expanded
+	return current
 }
 
-func compactFileSystemByFile(files []File) []File {
+func compactFileSystemByFile(file *File) {
 	moved := make(map[int]bool)
-	fileIndex := len(files) - 1
+
+	// find the start
+	start := beginningOfList(file)
+	toMove := endOfList(file)
+
 	for {
-		fileId := files[fileIndex].id
-		if moved[fileId] {
-			break
-		}
-		files = moveFileIfPossible(files, fileIndex)
-		// fmt.Println(expandFileSystemTwo(files))
-		moved[fileId] = true
-
-		// get next file to move
-		for i := len(files) - 1; i > 0; i-- {
-			file := files[i]
-			if file.id == -1 || moved[file.id] {
-				continue
+		current := start
+		for {
+			if current.id == toMove.id {
+				moved[toMove.id] = true
+				break
 			}
-			fileIndex = i
+
+			if current.id == -1 && current.length >= toMove.length {
+				// current File is free space with enough to fit toMove
+
+				// unpick toMove from it's current position
+				// and replace with empty space
+				next := toMove.next
+				prev := toMove.previous
+				space := &File{-1, toMove.length, nil, nil}
+				if prev != nil {
+					prev.next = space
+					space.previous = prev
+				}
+				if next != nil {
+					next.previous = space
+					space.next = next
+				}
+				toMove.next = nil
+				toMove.previous = nil
+
+				current.previous.next = toMove
+				toMove.previous = current.previous
+
+				if current.length > toMove.length {
+					toMove.next = &File{-1, current.length - toMove.length, toMove, current.next}
+					current.next.previous = toMove.next
+				} else {
+					toMove.next = current.next
+					current.next.previous = toMove
+				}
+
+				// orphan current
+				current.next = nil
+				current.previous = nil
+				moved[toMove.id] = true
+				break
+			}
+
+			current = current.next
+		}
+
+		toMove = endOfList(toMove)
+		// fmt.Println(expandFileSystemTwo(toMove))
+
+		for {
+			if toMove.id != -1 && !moved[toMove.id] {
+				break
+			}
+			toMove = toMove.previous
+			if toMove == nil {
+				break
+			}
+		}
+
+		if toMove == nil {
 			break
 		}
 	}
-	return files
 }
 
-func moveFileIfPossible(files []File, index int) []File {
-	fileToMove := files[index]
-	newFileSystem := []File{}
-	moved := false
-	for i := range files {
-		current := files[i]
-		if !moved && current.id == -1 && current.length >= fileToMove.length && i < index {
-			newFileSystem = append(newFileSystem, File{fileToMove.id, fileToMove.length})
-			if fileToMove.length < current.length {
-				newFileSystem = append(newFileSystem, File{-1, current.length - fileToMove.length})
-			}
-			moved = true
-		} else if current.id == fileToMove.id && moved {
-			newFileSystem = append(newFileSystem, File{-1, fileToMove.length})
-		} else {
-			newFileSystem = append(newFileSystem, File{current.id, current.length})
+func beginningOfList(file *File) *File {
+	current := file
+	for {
+		if current.previous == nil {
+			break
 		}
+		current = current.previous
 	}
-	return newFileSystem
+	return current
 }
 
-func expandFileSystemTwo(files []File) []string {
+func endOfList(file *File) *File {
+	current := file
+	for {
+		if current.next == nil {
+			break
+		}
+		current = current.next
+	}
+	return current
+}
+
+func expandFileSystemTwo(file *File) []string {
 	expanded := []string{}
-	for i := range files {
-		file := files[i]
-		content := strconv.Itoa(file.id)
-		if file.id == -1 {
+
+	// find the beginning of the list
+	current := beginningOfList(file)
+
+	for {
+		if current == nil {
+			break
+		}
+		content := strconv.Itoa(current.id)
+		if current.id == -1 {
 			content = "."
 		}
-		for range file.length {
+		for range current.length {
 			expanded = append(expanded, content)
 		}
+		current = current.next
 	}
+
 	return expanded
 }
 
@@ -152,12 +217,12 @@ func PartTwo(path string) {
 	fmt.Println("Day 9 - Part 2: ")
 	data := utils.ReadFile(path)
 	line := strings.Split(strings.Split(data, "\n")[0], "")
-	expanded := expandFileSystemToFiles(line)
-	// fmt.Println(expandFileSystemTwo(expanded))
-	compacted := compactFileSystemByFile(expanded)
+	file := expandFileSystemToFiles(line)
+	// fmt.Println(expandFileSystemTwo(file))
+	compactFileSystemByFile(file)
+	// fmt.Println(expandFileSystemTwo(file))
 
-	checksum := calculateCheckSum(expandFileSystemTwo(compacted))
-	// fmt.Println(expandFileSystemTwo(compacted))
+	checksum := calculateCheckSum(expandFileSystemTwo(file))
 
 	fmt.Println("Checksum:", checksum)
 }
