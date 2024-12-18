fc5b5343f1dba0da3a7f2648c740c8a9bab1dd61
Johanna Pearce <jo@nrwl.io>
2024-12-18 12:55:16 +0000

    2024-12-16, day 16, part two

--- day16/day16.go
+++ day16/day16.go
@@ -2,6 +2,7 @@ package day16
 
 import (
 	"fmt"
+	"math"
 	"sort"
 	"strings"
 )
@@ -103,7 +104,6 @@ func PartOne(data string) {
 	for {
 		current := frontier[0]
 		frontier = frontier[1:]
-
 		if current.point.x == end.x && current.point.y == end.y {
 			break
 		}
@@ -149,6 +149,15 @@ func parseInput(data string) Grid {
 	return grid
 }
 
+type Cost struct {
+	cost int
+	from []FrontierPoint
+}
+
+func getFaceKey(x int, y int, face Direction) string {
+	return fmt.Sprintf("%d:%d:%d", x, y, face)
+}
+
 func PartTwo(data string) {
 	fmt.Println("Day 16 - Part 2: ")
 	grid := parseInput(data)
@@ -157,5 +166,95 @@ func PartTwo(data string) {
 		panic("Start not found")
 	}
 
-	fmt.Println(len(grid))
+	end := Point{len(grid[0]) - 2, 1}
+	if grid[end.y][end.x] != "E" {
+		panic("End not found")
+	}
+
+	costSoFar := map[string]*Cost{}
+
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] != "#" {
+				costSoFar[getFaceKey(x, y, North)] = &Cost{math.MaxInt32, []FrontierPoint{}}
+				costSoFar[getFaceKey(x, y, South)] = &Cost{math.MaxInt32, []FrontierPoint{}}
+				costSoFar[getFaceKey(x, y, East)] = &Cost{math.MaxInt32, []FrontierPoint{}}
+				costSoFar[getFaceKey(x, y, West)] = &Cost{math.MaxInt32, []FrontierPoint{}}
+			}
+		}
+	}
+
+	// set the start square to zero cost
+	costSoFar[getFaceKey(start.x, start.y, East)] = &Cost{0, []FrontierPoint{}}
+
+	frontier := []FrontierPoint{{start, 0, East}}
+	for len(frontier) > 0 {
+		current := frontier[0]
+		frontier = frontier[1:]
+		if current.point.x == end.x && current.point.y == end.y {
+			// we've found one way
+			continue
+		}
+
+		for face, d := range directions {
+			x1 := current.point.x + d.x
+			y1 := current.point.y + d.y
+
+			if grid[y1][x1] == "#" {
+				continue
+			}
+
+			nextKey := getFaceKey(x1, y1, face)
+			currentKey := getFaceKey(current.point.x, current.point.y, current.facing)
+			currentCost := costSoFar[currentKey]
+
+			newCost := currentCost.cost + getCost(face, current.facing)
+			existingCost := costSoFar[nextKey]
+
+			if existingCost.cost == newCost {
+				// add current point to from list
+				existingCost.from = append(existingCost.from, current)
+				continue
+			}
+
+			if newCost < existingCost.cost {
+				costSoFar[nextKey] = &Cost{newCost, []FrontierPoint{{Point{current.point.x, current.point.y}, current.cost, current.facing}}}
+				frontier = append(frontier, FrontierPoint{Point{x1, y1}, newCost, face})
+				sort.Slice(frontier, func(i, j int) bool {
+					return frontier[i].cost < frontier[j].cost
+				})
+			}
+		}
+	}
+
+	// work back from the end and fill in all the paths?
+	points := []FrontierPoint{{Point{end.x, end.y}, 0, North}}
+	for len(points) > 0 {
+		current := points[0]
+		points = points[1:]
+		grid[current.point.y][current.point.x] = "O"
+		currentCost := costSoFar[getFaceKey(current.point.x, current.point.y, current.facing)]
+		if len(currentCost.from) > 0 {
+			points = append(points, currentCost.from...)
+		}
+	}
+
+	count := 0
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == "O" {
+				count++
+			}
+		}
+	}
+
+	// printGrid(grid)
+
+	fmt.Println("Number of tiles:", count)
+}
+
+func printGrid(grid Grid) {
+	for y := range grid {
+		fmt.Println(strings.Join(grid[y], ""))
+	}
 }
